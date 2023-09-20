d269c888e08b9237bc833054e00eab60c8fbe066
HOFi <145539219+HOFii@users.noreply.github.com>
2023-09-20 15:06:56 +0700

    Create Biodata.kt

---
+++ Biodata.kt
@@ -0,0 +1,10 @@
+fun main()
+{
+  var nama  = "Gusti Alifiraqsha Akbar"
+  var kelas = "XI RPL 2"
+  var hobi  = "Baca LN (≧▽≦)"
+
+  println("Nama: $nama")
+  println("Kelas: $kelas")
+  println("Hobi: $hobi")
+}
